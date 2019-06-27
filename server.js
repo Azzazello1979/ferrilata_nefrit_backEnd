6a413ebd7ae635841f44756a23405e487c01f784
Ruszin Peter <ruszin.peter@gmail.com>
2019-06-27 14:14:05 +0200

    add server.js /channels endpoint

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const mongoDbServer = '127.0.0.1:27017';
+const mongoDatabase = '';
+const mongoCollection = '';
+
+app.get('/channels', (req, res) => {
+    mongoose.connect(`mongodb://${mongoDbServer}/${ mongoDatabase}`, { useNewUrlParser: true }, (err, response) => {
+        if (err) {
+            return res.status(500).json({ "message": "Something went wrong, please try again later." });
+        } else {
+            console.log('mongodb connected');
+            const collection = response.db.collection(mongoCollection);
+            collection.find().toArray((err, items) => {
+                if (err) throw err;
+                res.status(200).json(items);
+            });
+        };
+        response.close(console.log('mongodb closed'));
+    });
+});
