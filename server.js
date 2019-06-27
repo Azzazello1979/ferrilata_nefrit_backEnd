04e03bdded6938d9a94f2991f64a6bd775af0959
Ruszin Peter <ruszin.peter@gmail.com>
2019-06-27 13:58:20 +0200

    modified mongodb connection with mongoose

--- server.js
+++ server.js
@@ -3,27 +3,28 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const mongo = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
+const mongoose = require('mongoose');
+const mongoDbServer = '127.0.0.1:27017';
+const mongoDatabase = '';
+const mongoCollection = '';
 
 app.get('/posts', (req, res) => {
-    mongo.connect((url), (err, client) => {
+    mongoose.connect(`mongodb://${mongoDbServer}/${ mongoDatabase}`, { useNewUrlParser: true }, (err, response) => {
         if (err) {
             return res.status(500).json({ "message": "Something went wrong, please try again later." });
         } else {
-            console.log('database connected');
-            const db = client.db('TestDatabase');
-            const collection = db.collection('TestCollection');
+            console.log('mongodb connected');
+            const collection = response.db.collection(mongoCollection);
             collection.find().toArray((err, items) => {
                 if (err) throw err;
                 res.status(200).json(items);
             })
         }
-        client.close(console.log('closed'));
+        response.close(console.log('mongodb closed'));
     })
-});
+})
 
-const server = app.listen(port, (err) => {
+const backendServerStatus = app.listen(port, (err) => {
     if (err) {
         return console.log(err);
     }
