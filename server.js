f59b40e4b0eba83edd7f06bb405cff506ce56db0
Ruszin Peter <ruszin.peter@gmail.com>
2019-06-28 13:47:16 +0200

    moved port variable from server.js to .env

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 'use strict';
 
+require('dotenv').config('.env');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config('.env');
+const port = process.env.port;
+const mongoDbServer = process.env.mongoDbServer;
+const mongoDatabase = process.env.mongoDatabase;
+const mongoCollection = process.env.mongoCollection;
 
 app.get('/channels', (req, res) => {
-    mongoose.connect(`mongodb://${mongoDbServer}/${ mongoDatabase}`, { useNewUrlParser: true }, (err, response) => {
+    mongoose.connect(`mongodb://${mongoDbServer}/${mongoDatabase}`, { useNewUrlParser: true }, (err, response) => {
         if (err) {
             return res.status(500).json({ "message": "Something went wrong, please try again later." });
         } else {
-            console.log('mongodb connected');
             const collection = response.db.collection(mongoCollection);
             collection.find().toArray((err, items) => {
-                if (err) throw err;
+                //if (err) throw err;
                 res.status(200).json(items);
             });
         };
-        response.close(console.log('mongodb closed'));
+        response.close();
     });
+});
+
+const backendServerStatus = app.listen(port, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    return console.log(`Server listening on port ${port}`);
 });
