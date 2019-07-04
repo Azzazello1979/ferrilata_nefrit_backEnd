fe6a14a924c944be97c13d913ec94c4e7405fde4
Soma <soma.feigl@gmail.com>
2019-07-04 10:11:00 +0200

    added passport WIP

--- server.js
+++ server.js
@@ -1,41 +1,87 @@
 'use strict';
 
-// require('dotenv').config('.env');
-// const port = process.env.port;
-// const mongoDbServer = process.env.mongoDbServer;
-// const mongoDatabase = process.env.mongoDatabase;
-// const mongoCollection = process.env.mongoCollection;
+require('dotenv').config('.env');
+const port = process.env.port;
+const mongoDatabase = process.env.mongoDatabase;
+const mongoCollection = process.env.mongoCollection;
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://clairvoyant:myfirstmongoDB@cluster0-0pada.mongodb.net/test?retryWrites=true&w=majority";
+const key = process.env.key
+const uri = process.env.uri;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 const mongoose = require('mongoose');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
-const port = 3200;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const refreshTokens = {};
+const passportOpts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: key
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new JwtStrategy(passportOpts, function (jwtPayload, done) {
+    const expirationDate = new Date(jwtPayload.exp * 1000);
+    if (expirationDate < new Date()) {
+        return done(null, false);
+    }
+    done(null, jwtPayload);
+}))
+
+passport.serializeUser(function (user, done) {
+    done(null, user.username)
+});
+
+app.post('/logout', function (req, res) {
+    const refreshToken = req.body.refreshToken;
+    if (refreshToken in refreshTokens) {
+        delete refreshTokens[refreshToken];
+    }
+    res.sendStatus(204);
+});
+
+app.post('/refresh', function (req, res) {
+    const refreshToken = req.body.refreshToken;
+    if (refreshToken in refreshTokens) {
+        const user = { 'username': refreshTokens[refreshToken], }
+        const token = jwt.sign(user, key, { expiresIn: 600 });
+        res.json({ jwt: token })
+    }
+    else {
+        res.sendStatus(401);
+    }
+});
+
+app.get('/', passport.authenticate('jwt'), (req, res) => {
     mongo.connect((url), (err, client) => {
         if (err) {
             return res.status(500).json({ 'message': 'Something went wrong, please try again later.' });
         } else {
-            const db = client.db('TestDatabase');
-            const collection = db.collection('TestCollection');
+            const db = client.db(mongoDatabase);
+            const collection = db.collection('PostCollection');
             collection.find().toArray((err, items) => {
                 if (err) throw err;
                 res.status(200).json(items);
             })
         }
-        client.close();
+        // client.close();
     })
 });
 
 
 //Define a schema
-// const User = require('../ferrilata-nefrit-frontend/Reddit/src/app/user.model.ts');
+
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     _id: String,
@@ -53,7 +99,7 @@ app.post('/login', (req, res) => {
         if (err) {
             return res.status(500).json({ 'message': 'Something went wrong, please try again later.' });
         }
-        const collection = client.db("first-test").collection("users");
+        const collection = client.db(mongoDatabase).collection(mongoCollection);
         collection.find({ username: `${req.body.username}`, password: `${req.body.password}` }).toArray((err, items) => {
             if (err) {
                 return res.status(500).json({ 'message': 'Something went wrong, please try again later.' });
@@ -63,12 +109,12 @@ app.post('/login', (req, res) => {
                         'message': 'Wrong username or password.'
                     });
                 } else {
-                    let resData = {
-                        '_id': items[0]._id,
-                        'username': items[0].username,
-                        'token': items[0].token,
-                        'refreshToken': items[0].refreshToken
-                    }
+                    let rToken = jwt.sign({ id: items[0]._id, username: items[0].username }, key, { expiresIn: '30d' });
+                    let token = jwt.sign({ id: items[0]._id, username: items[0].username }, key, { expiresIn: '1hr' });
+                    refreshTokens[rToken] = req.body.username;
+                    let resData = { '_id': items[0]._id, 'username': items[0].username, tokens: { 'jwt': token, 'refreshToken': rToken } }
+                    collection.update({ username: `${req.body.username}` },
+                        { $set: { refreshToken: `${rToken}` } })
                     return res.status(200).json(resData);
                 }
             }
