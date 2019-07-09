dec5ca7c5dd9233bdf82daa266a03fc571743e2b
Azzazello1979 <balint.haui@gmail.com>
2019-07-09 12:21:16 +0200

    dotenv installed, db credentials and secret jwt signature key moved to .env file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config('.env')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,7 +6,11 @@ const jwt = require('jsonwebtoken')
 const {User} = require('models/user')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const PORT = 3000
+const PORT = process.env.port
+const secretKey = process.env.secretKey
+const mongoUser = process.env.mongoUser
+const mongoPassword = process.env.mongoPassword
+const mongoDatabase = process.env.mongoDatabase
 
 
 app.use(cors())
@@ -16,7 +21,7 @@ app.get('/', (req, res) => {
   res.status(200).send("OK, backend connected to frontend.")
 })
 
-const db = "mongodb+srv://userBalint:passwordBalint@cluster0-siax1.mongodb.net/nefrit?retryWrites=true&w=majority"
+const db = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0-siax1.mongodb.net/${mongoDatabase}?retryWrites=true&w=majority`
 
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('OK...connected to mongoDB'))
@@ -26,13 +31,32 @@ mongoose.connect(db, { useNewUrlParser: true })
 // REGISTER new user end point
 app.post('/register', (req, res) => {
   let userData = req.body;
-  let refreshToken = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15); // 22 chars. long rand.string
+  let startRefreshToken = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15); // 22 chars. long rand.string
+  let newUserPayload = {
+    "username": user.Data.username,
+    "password": user.Data.password
+  }
+  let startAccessToken = jwt.sign(newUserPayload, secretKey, { expiresIn: '300' }) // 5 mins.
+
   User.findOne({ username: userData.username }) //look up in database if such username is already registered
   .then( (user) => {
     if(user){ // if we already have such username...
       res.status(400).json({"message": "Username is already taken."})
     } else { // ..if not, let's register user...
-      let user = new User(userData)
+      let user = new User({
+        "username": user.Data.username,
+        "password": user.Data.password,
+        "refreshToken": startRefreshToken
+      })
+      user.save((err , registeredUser) => {
+        if(err){
+          console.log('database error saving user' + err)
+          res.status(500).json({"message": "database error saving user"})
+          return 
+        } else {
+
+        }
+      })
     }
   }) // 500 internal server error
   .catch( (err) => res.status(500).json({"message": "Something went wrong, please try again later."}))
