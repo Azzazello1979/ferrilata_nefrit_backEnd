f4c2b586b9869272eb833a36c24c4be357c1ba1b
Azzazello1979 <balint.haui@gmail.com>
2019-07-09 12:39:57 +0200

    http headers added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
-const {User} = require('models/user')
+const { User } = require('models/user')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const PORT = process.env.port
@@ -32,34 +32,41 @@ mongoose.connect(db, { useNewUrlParser: true })
 app.post('/register', (req, res) => {
   let userData = req.body;
   let startRefreshToken = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15); // 22 chars. long rand.string
+
   let newUserPayload = {
     "username": user.Data.username,
     "password": user.Data.password
   }
+  
   let startAccessToken = jwt.sign(newUserPayload, secretKey, { expiresIn: '300' }) // 5 mins.
 
   User.findOne({ username: userData.username }) //look up in database if such username is already registered
-  .then( (user) => {
-    if(user){ // if we already have such username...
-      res.status(400).json({"message": "Username is already taken."})
-    } else { // ..if not, let's register user...
-      let user = new User({
-        "username": user.Data.username,
-        "password": user.Data.password,
-        "refreshToken": startRefreshToken
-      })
-      user.save((err , registeredUser) => {
-        if(err){
-          console.log('database error saving user' + err)
-          res.status(500).json({"message": "database error saving user"})
-          return 
-        } else {
-
-        }
-      })
-    }
-  }) // 500 internal server error
-  .catch( (err) => res.status(500).json({"message": "Something went wrong, please try again later."}))
+    .then((user) => {
+      if (user) { // if we already have such username...
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).json({ "message": "Username is already taken." })
+      } else { // ..if not, let's register user...
+        let user = new User({
+          "username": user.Data.username,
+          "password": user.Data.password,
+          "refreshToken": startRefreshToken
+        })
+        user.save((err, registeredUser) => {
+          if (err) {
+            console.log('database error saving user' + err)
+            res.setHeader("Content-Type", "application/json")
+            res.status(503).json({ "message": "Error saving user to database." })
+            return
+          } else {
+            res.setHeader("Content-Type", "application/json")
+            res.status(200).send(registeredUser)
+          }
+        })
+      }
+    }) // 500 internal server error
+    .catch((err) =>
+      res.setHeader("Content-Type", "application/json"),
+      res.status(500).json({ "message": "Something went wrong, please try again later." }))
 
 })
 
