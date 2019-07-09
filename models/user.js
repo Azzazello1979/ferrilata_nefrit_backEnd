939e59b08c8a1e3e73ed278259ef04263f4b524b
Azzazello1979 <balint.haui@gmail.com>
2019-07-09 11:07:42 +0200

    user model created

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.model('User', new mongoose.Schema({
+  username: {
+    type:String,
+    required:true,
+    unique:true
+    },
+  password: {
+    type:String,
+    required:true,
+    },
+  refreshToken: {
+    type: String
+  }
+}))
+
+exports.User = User
+
+
