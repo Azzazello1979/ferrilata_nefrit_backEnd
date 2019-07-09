f4c25f53b92e408e19554c09f04b18bfc653751f
Azzazello1979 <balint.haui@gmail.com>
2019-07-09 11:08:35 +0200

    User model imported

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('bodyParser')
+const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const User = require('models/user')
 const mongoose = require('mongoose')
@@ -8,15 +8,25 @@ const cors = require('cors')
 const PORT = 3000
 
 
+
+
 app.use(cors())
 app.use(bodyParser.json())
 
 
 
-
+// TEST connection
 app.get('/' , (req,res) => {
   res.status(200).send("OK, backend connected to frontend.")
 })
+
+
+
+
+
+
+
+
 
 app.listen(PORT, ()=>{
   console.log(`OK...Express listening on ${PORT}`)
