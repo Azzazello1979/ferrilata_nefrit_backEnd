e1eb2f71562c7b5a0ef1e573fc194d046b3102ec
Azzazello1979 <balint.haui@gmail.com>
2019-07-10 16:52:58 +0200

    tested OK

--- server.js
+++ server.js
@@ -38,63 +38,64 @@ mongoose.connect(db, { useNewUrlParser: true })
 // refresh access token backend
 app.post('/refresh-token', (req,res) => {
 
+
+
   // 3. missing content type 400
   if (req.headers["content-type"] !== 'application/json') {
-  res.status(400).json({ "message": "Content-type is not specified." });
-  return;
+  return res.status(400).json({ "message": "Content-type is not specified." });
   }
 
   // 5. Missing refreshToken property from request body 400
   if (!req.body.refreshToken) {
   res.setHeader("Content-Type", "application/json");
-  res.status(400).json({ "message": "Missing refreshToken." });
-  return;
+  return res.status(400).json({ "message": "Missing refreshToken." });
   }
 
   // check if we have such refresh-token
   User.findOne({refreshToken : req.body.refreshToken})
   .then((user) => {
-    // such refresh-token exists...now lets see if its a valid one or not
+    if(user){ // such refresh-token exists...now lets see if its a valid one or not
+    
     jwt.verify(user.refreshToken, secret, (err) => {
-      if(err){
-        res.status(401).json({ "message" : "refresh-token is invalid" });
-        return;
-      } else {
+      
+      const decoded = jwt.decode(user.refreshToken);
+
+      if(err){ // refresh-token is not valid
+        return res.status(401).json({ "message" : "refresh-token is invalid" });
         
       // valid, lets check expiry
-      const decoded = jwt.decode(user.refreshToken)
-      if(decoded.exp < new Date()){
-        // expired!
+      } else if (decoded.exp < new Date()){ // expired!
+        
         res.setHeader("Content-Type", "application/json");
-        res.status(401).json({"message" : "refresh-token is expired"});
-        return;
+        return res.status(401).json({"message" : "refresh-token is expired"});
+      } else { // valid & not expired, lets generate new access-token & send to frontend
 
-
-      } else {
-      // valid & not expired, lets generate new access-token & send to frontend
         const userPayload = ({
           username: user.username,
           password: user.password
         })
+
         const newAccessToken = jwt.sign(userPayload, secret, {expiresIn: '300'}); //5 mins
         res.setHeader("Content-Type", "application/json");
-        res.status(200).json({ "token" : newAccessToken })
+        return res.status(200).json({ "token" : newAccessToken });
       }
+    })
 
-      }
-      })
+    
 
+
+    }else{
+      return res.status(400).json({"message":"user with that refresh-token does not exist"})
+    }
+
+  }).catch((err) => console.log('Database error: ' + err));
 
   })
 
-  // internal server error 500
-  .catch((err) =>
-    console.log('Database error: ' + err),
-    res.setHeader("Content-Type", "application/json"),
-    res.status(500).json({"message": "Something went wrong, please try again later."})
-  );
+  
 
-})
+
+
 
 
 
