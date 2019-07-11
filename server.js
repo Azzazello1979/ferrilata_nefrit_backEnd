0c019263d62f42d2d1673e6c90b560299b6a52d8
Azzazello1979 <balint.haui@gmail.com>
2019-07-11 11:37:04 +0200

    register end point moved to ./routes/register.js

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ require('dotenv').config('.env');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const User = require('./models/user');
+
+
 const mongoose = require('mongoose');
 const cors = require('cors');
 const PORT = process.env.port;
-const secret = process.env.secret;
+
 const mongoUser = process.env.mongoUser;
 const mongoPassword = process.env.mongoPassword;
 const mongoDatabase = process.env.mongoDatabase;
 
+const registerRoute = require('./routes/register');
 
+//Routes
+app.use('/register', registerRoute);
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -28,84 +31,7 @@ mongoose.connect(db, { useNewUrlParser: true })
 
 
 
-// REGISTER new user end point
-app.post('/register', (req, res) => {
 
-  // missing content type, 400 error
-  if (req.headers["content-type"] !== 'application/json') {
-    res.setHeader("Content-Type", "application/json");
-    res.status(400).json({ "message": "Content-type is not specified." });
-    return;
-  }
-
-
-  // missing property from req. body, 400 error
-  if (!req.body.username && !req.body.password) {
-    res.setHeader("Content-Type", "application/json");
-    res.status(400).json({ "message": "Missing username and password" });
-    return;
-
-  } else if (!req.body.username) {
-
-    res.setHeader("Content-Type", "application/json");
-    res.status(400).json({ "message": "Missing username" });
-    return;
-
-  } else if (!req.body.password) {
-    res.setHeader("Content-Type", "application/json");
-    res.status(400).json({ "message": "Missing password" });
-    return;
-  }
-
-  const userData = req.body;
-  
-
-  const newUserPayload = ({
-    username: userData.username,
-    password: userData.password
-  });
-
-  const startAccessToken = jwt.sign( newUserPayload , secret, { expiresIn: '300' }); // 5 mins.
-
-  const startRefreshToken = jwt.sign( newUserPayload , secret, { expiresIn: '30d' }); // 30 days.
-
-  User.findOne({ username: userData.username }) //look up in database if such username is already registered
-    .then((user) => {
-      if (user) { // if we already have such username...
-        res.setHeader("Content-Type", "application/json");
-        res.status(400).json({ "message": "Username is already taken." });
-      } else { // ..if not, let's register user...
-        let user = new User({
-          "password": userData.password,
-          "username": userData.username,
-          "refreshToken": startRefreshToken
-        });
-        user.save((err, registeredUser) => {
-          if (err) {
-            res.setHeader("Content-Type", "application/json");
-            res.status(503).json({ "message": "Error saving user to database." });
-            return;
-
-          } else {
-            res.setHeader("Content-Type", "application/json");
-            res.status(200).json({
-              "_id": registeredUser._id,
-              "username": registeredUser.username,
-              "token": startAccessToken,
-              "refreshToken": registeredUser.refreshToken
-            });
-          }
-        })
-      }
-    }) 
-    .catch( // 500 internal server error
-      (err) => res.status(500)
-      .json({"message": "Something went wrong, please try again later." })
-      .console.log('Error '+ err)
-    );
-
-
-})
 
 
 
