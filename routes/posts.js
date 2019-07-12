69a32f4e35f7a2bf804b76d5ce5317b6c1c69e82
Soma <soma.feigl@gmail.com>
2019-07-12 15:12:30 +0200

    Refactored posts.js for FAN-17

--- routes/posts.js
+++ routes/posts.js
@@ -6,13 +6,42 @@ const Posts = mongoose.model('Post', PostSchema);
 const UserSchema = require('./../models/user');
 const Users = mongoose.model('User', UserSchema);
 
-router.get('/', (req, res) => {//  posts/:channel?
-
-  Users.find({},
-    (err, items1) => {
-      const users = items1;
-      Posts.find({},
-        (err, items2) => {
+router.get('/:channel?', (req, res) => {
+  if (!req.params.channel) {
+    Users.find({},
+      (err, items1) => {
+        const users = items1;
+        Posts.find({},
+          (err, items2) => {
+            const posts = items2;
+            let sendBackData = [];
+            for (let i = 0; i < posts.length; i++) {
+              for (let j = 0; j < users.length; j++) {
+                if (posts[i].userId.equals(users[j]._id)) {
+                  let payload = {
+                    _id: posts[i]._id,
+                    title: posts[i].title,
+                    content: posts[i].content,
+                    channel: posts[i].channel,
+                    timestamp: posts[i].timestamp,
+                    username: users[j].username
+                  };
+                  sendBackData.push(payload);
+                }
+              }
+            }
+            res.setHeader("Content-Type", "application/json");
+            res.status(200).json(sendBackData);
+          });
+      })
+  } else {
+    Users.find({},
+      (err, items1) => {
+        const users = items1;
+        Posts.find({ channel: req.params.channel }, (err, items2) => {
+          if (err) {
+            return res.json({ "message": "No such channel" })
+          };
           const posts = items2;
           let sendBackData = [];
           for (let i = 0; i < posts.length; i++) {
@@ -32,8 +61,10 @@ router.get('/', (req, res) => {//  posts/:channel?
           }
           res.setHeader("Content-Type", "application/json");
           res.status(200).json(sendBackData);
-        });
-    })
+        })
+      })
+  }
 })
+
 
 module.exports = router;
