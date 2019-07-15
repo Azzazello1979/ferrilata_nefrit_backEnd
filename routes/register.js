a0dd6fb424834db3858eac1cb0ef25b2972ddc21
Azzazello1979 <balint.haui@gmail.com>
2019-07-15 20:12:51 +0200

    renamed some vars for clarity

--- routes/register.js
+++ routes/register.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const userSchema = require('./../models/user');
-const Users = mongoose.model('User', userSchema , 'users');
-
+const User = mongoose.model('User', userSchema, 'users');
 const key = process.env.key;
+
 
 // body-parser is needed to populare req.body!
 const bodyParser = require('body-parser');
@@ -26,37 +26,36 @@ router.post('/', (req, res) => {
   if (!req.body.username && !req.body.password) {
     res.setHeader("Content-Type", "application/json");
     return res.status(400).json({ "message": "Missing username and password" });
-    
+
 
   } else if (!req.body.username) {
 
     res.setHeader("Content-Type", "application/json");
     return res.status(400).json({ "message": "Missing username" });
-    
+
 
   } else if (!req.body.password) {
     res.setHeader("Content-Type", "application/json");
     return res.status(400).json({ "message": "Missing password" });
-    
+
   }
 
   const userData = req.body;
-  
+
   const newUserPayload = ({
     username: userData.username
   });
 
-  const startAccessToken = jwt.sign( newUserPayload , key, { expiresIn: '300' }); // 5 mins.
-  const startRefreshToken = jwt.sign( newUserPayload , key, { expiresIn: '30d' }); // 30 days.
+  const startAccessToken = jwt.sign(newUserPayload, key, { expiresIn: '300' }); // 5 mins.
+  const startRefreshToken = jwt.sign(newUserPayload, key, { expiresIn: '30d' }); // 30 days.
 
-//look up in database if such username is already registered
-  Users.findOne({ username: userData.username })
-    .then((existingUser) => {
-      if (existingUser) { // if we already have such username...
+  //look up in database if such username is already registered
+  User.findOne({ username: userData.username }) //look up in database if such username is already registered
+    .then((registeredUser) => {
+      if (registeredUser) { // if we already have such username...
         res.setHeader("Content-Type", "application/json");
         res.status(400).json({ "message": "Username is already taken." });
       } else { // ..if not, let's register user...
-
         let newUser = new User({
           "password": userData.password,
           "username": userData.username,
@@ -67,7 +66,6 @@ router.post('/', (req, res) => {
             res.setHeader("Content-Type", "application/json");
             res.status(503).json({ "message": "Error saving user to database." });
             return;
-
           } else {
             res.setHeader("Content-Type", "application/json");
             res.status(200).json({
@@ -79,14 +77,12 @@ router.post('/', (req, res) => {
           }
         })
       }
-    }) 
+    })
     .catch( // 500 internal server error
-    (err) => res.status(500)
-    .json({"message": "Something went wrong, please try again later." })
-    .console.log('Database error: ' + err)
-    )
-
-
+      (err) => res.status(500)
+        .json({ "message": "Something went wrong, please try again later." })
+        .console.log('Error ' + err)
+    );
 })
 
 module.exports = router;
