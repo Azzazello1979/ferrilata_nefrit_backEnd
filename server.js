6252c66e0ba269514786e8652dad91472fae051d
Azzazello1979 <balint.haui@gmail.com>
2019-07-15 14:24:41 +0200

    mongoose.set('useCreateIndex', true); is needed to set uniq:true in schemas

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config('.env');
 const port = process.env.port;
 const uri = process.env.uri
+const uri2 = process.env.uri2
 const registerRoute = require('./routes/register');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -19,15 +20,19 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//Mongoose stop deprecation warning - needed to use unique:true @ schemas
+mongoose.set('useCreateIndex', true);
 
 //Mongoose connection
-const db = mongoose.connect(uri, { useNewUrlParser: true })
+const db = mongoose.connect(uri2, { useNewUrlParser: true })
   .then(() => {
     console.log("Connected to db")
   })
   .catch(() => {
     console.log("Connection failed")
   });
+
+
 
 
 app.listen(port, (err) => {
