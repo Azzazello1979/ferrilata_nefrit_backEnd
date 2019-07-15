27281ce2d34451ed5e4585eb9c9a63869a1ea984
Azzazello1979 <balint.haui@gmail.com>
2019-07-15 11:53:22 +0200

    un-needed routes removed AND app.use(cors()); is placed ahead of app.use('/register', registerRoute);

--- server.js
+++ server.js
@@ -4,26 +4,17 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config('.env');
 const port = process.env.port;
-const loginRoute = require('./routes/login');
-const logoutRoute = require('./routes/logout');
-const channelsRoute = require('./routes/channels');
-const postsRoute = require('./routes/posts');
+const uri = process.env.uri
 const registerRoute = require('./routes/register');
-const refreshTokenRoute = require('./routes/refresh-token');
-const middleware = require('./middleware'); // TO BE USED LATER
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const uri = process.env.uri
+
 
 //Routes
-app.use('/login', loginRoute);
-app.use('/logout', logoutRoute);
-app.use('/channels', channelsRoute);
-app.use('/posts', postsRoute);
-app.use('/refresh-token', refreshTokenRoute);
+app.use(cors());
+
 app.use('/register', registerRoute);
 app.use(bodyParser.json());
-app.use(cors());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
