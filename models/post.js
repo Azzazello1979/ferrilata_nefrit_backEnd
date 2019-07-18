846fd7748b1c3672d52754d6e1aaf9ce2ce4b48d
Szabolcs Veer <szabolcs.veer@gmail.com>
2019-07-18 09:49:30 +0200

    add(post.js) created upvote and downvote properties to post schema

--- models/post.js
+++ models/post.js
@@ -1,28 +1,36 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  title: {
-    type: String,
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  channel: {
-    type: String,
-    required: true
-  },
-  timestamp: {
-    type: Number,
-    required: true
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  }
+    _id: mongoose.Schema.Types.ObjectId,
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    channel: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Number,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    upVote: {
+        type: Array,
+        required: true
+    },
+    downVote: {
+        type: Array,
+        required: true
+    }
 });
 
 module.exports = postSchema;
