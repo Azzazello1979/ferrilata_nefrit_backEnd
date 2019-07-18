02c5901f1d3469f5f0e8023575851d1235095ee3
Szabolcs Veer <szabolcs.veer@gmail.com>
2019-07-18 14:41:29 +0200

    fix(posts.js) delete endpoint got fixed and error handling rearranged

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,13 @@ const PostSchema = require('./../models/post');
 const Posts = mongoose.model('Post', PostSchema);
 const UserSchema = require('./../models/user');
 const Users = mongoose.model('User', UserSchema);
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const jwt = require('jsonwebtoken');
+const key = process.env.key;
 
+const middleware = require('../middleware'); // MIGHT USE
 
 router.get('/:channel?', (req, res) => {
     if (!req.params.channel) {
@@ -36,9 +42,13 @@ router.get('/:channel?', (req, res) => {
     } else {
         Users.find({},
             (err, users) => {
-                Posts.find({ channel: req.params.channel }, (err, posts) => {
+                Posts.find({
+                    channel: req.params.channel
+                }, (err, posts) => {
                     if (err) {
-                        return res.json({ "message": "No such channel" })
+                        return res.json({
+                            "message": "No such channel"
+                        })
                     };
                     let sendBackData = [];
                     for (let i = 0; i < posts.length; i++) {
@@ -65,30 +75,47 @@ router.get('/:channel?', (req, res) => {
 
 router.delete('/:postId', (req, res) => {
     let userId = '0';
+    console.log(req.params.postId);
     if (!req.headers['authorization']) { // MISSING TOKEN
-        return res.status(401).json({ "message": "You are not authenticated." })
+        return res.status(401).json({
+            "message": "You are not authenticated."
+        })
     } else if (req.headers['authorization']) {
         let token = req.headers['authorization'];
         token = token.slice(7, token.length);
         jwt.verify(token, key, (err) => {
             if (err) { // NOT VALID
-                return res.status(401).json({ "message": "You are not authenticated...." })
+                return res.status(401).json({
+                    "message": "You are not authenticated...."
+                })
             } else {
                 let decoded = jwt.decode(token);
                 userId = decoded._id
+                Posts.find({
+                    _id: req.params.postId,
+                    userId: userId
+                }, (err, posts) => {
+                    if (err) { // INTERNAL SERVER ERROR
+                        return res.status(500).json({
+                            'message': 'Something went wrong, please try again later.'
+                        })
+                    } else {
+                        Posts.findOneAndRemove({
+                            _id: req.params.postId
+                        }, (err, deletedPost) => {
+                            return res.status(200).json({
+                                deletedPost
+                            })
+                        });
+                    }
+
+                })
             }
         })
     } else {
-        Posts.find({ _id: req.params.postId, userId: userId }, (err, posts) => {
-            if (err) { // INTERNAL SERVER ERROR
-                return res.status(500).json({ 'message': 'Something went wrong, please try again later.' });
-            } else if (posts.length < 1) {
-                return res.status(404).json({ "message": "There is no such post." })
-            } else {
-                Posts.findOneAndRemove({ _id: req.params.postId }, (err, deletedPost) => {
-                    return res.status(204).json({ deletedPost })
-                });
-            }
+        console.log(posts);
+        return res.status(404).json({
+            "message": "There is no such post."
         })
     }
 });
