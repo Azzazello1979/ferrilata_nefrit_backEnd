191f2bbfcb35a9c9392a95f0359d37d128a3b50c
ruszinpeter <47865081+ruszinpeter@users.noreply.github.com>
2019-08-01 17:28:25 +0200

    created working channel path to db (#25)

--- routes/channels.js
+++ routes/channels.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-const PostSchema = require('./../models/post');
-const Posts = mongoose.model('Post', PostSchema);
+const ChannelsSchema = require('./../models/channels');
+const Channels = mongoose.model('Channels', ChannelsSchema);
 
 router.get('/', (req, res) => {
-    Posts.find({}, (err, items) => {
+    Channels.find({}, (err, items) => {
         if (err) {
             res.json(err.toString());
             return;
         };
         res.setHeader("Content-Type", "application/json");
-        res.status(200).json([...new Set(items.map(posts => posts.channel))]);
+        res.status(200).json([...new Set(items.map(channels => channels.channel))]);
     });
 });
 
